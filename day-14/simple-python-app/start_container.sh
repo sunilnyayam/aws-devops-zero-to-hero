792a5932595a9be87b1103cc33df2d05cbcf82a6
sunilnyayam <71542560+sunilnyayam@users.noreply.github.com>
2023-07-19 12:34:50 +0530

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull sunilnayayam/simple-pyhton-flask-service:latest
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 sunilnayayam/simple-pyhton-flask-service:latest
